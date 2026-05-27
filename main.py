6ba56c6fb781bce8696c303aff78b8d015690931
0LucasMatheus <lucasmatheus5305@gmail.com>
2026-05-27 03:21:14 -0300

    imports e pinagem

--- main.py
+++ main.py
@@ -1,1 +1,85 @@
-print("Hello, ESP32!")
+from machine import Pin, PWM, ADC, I2C, time_pulse_us
+import time
+from ds1307 import DS1307
+from esp32_gpio_lcd import GpioLcd
+from nec import NEC_8
+
+##########  configuração dos pinos 
+
+# 4 sensores
+ntc_pin    = 39
+trig_pin   = 4
+echo_pin   = 5
+ldr_pin    = 35
+ir_pin     = 34
+
+# 4 atuadores
+buzzer_pin = 18
+rele_pin   = 13
+servo_pin  = 12
+mot1_pin   = 25
+mot2_pin   = 26
+mot3_pin   = 27
+mot4_pin   = 14
+
+# RTC 
+rtc_scl  = 32
+rtc_sda  = 33
+
+# LCD
+lcd_rs   = 19
+lcd_en   = 21
+lcd_d4   = 22
+lcd_d5   = 23
+lcd_d6   = 0
+lcd_d7   = 15
+
+# sensores
+ntc = ADC(Pin(ntc_pin))
+ntc.atten(ADC.ATTN_11DB)
+
+trig = Pin(trig_pin, Pin.OUT)
+echo = Pin(echo_pin, Pin.IN)
+
+ldr = ADC(Pin(ldr_pin))
+ldr.atten(ADC.ATTN_11DB)
+
+ir = Pin(ir_pin, Pin.IN)
+
+# atuadores
+buzzer = PWM(Pin(buzzer_pin), freq=440)
+buzzer.duty(0)
+
+rele = Pin(rele_pin, Pin.OUT)
+rele.value(0)
+
+servo = PWM(Pin(servo_pin), freq=50)
+servo.duty(0)
+
+mot1 = Pin(mot1_pin, Pin.OUT)
+mot2 = Pin(mot2_pin, Pin.OUT)
+mot3 = Pin(mot3_pin, Pin.OUT)
+mot4 = Pin(mot4_pin, Pin.OUT)
+
+# RTC - relogio
+i2c = I2C(0, scl=Pin(rtc_scl), sda=Pin(rtc_sda))
+rtc = DS1307(i2c)
+
+# LCD
+lcd = GpioLcd(
+    rs_pin=Pin(lcd_rs),
+    enable_pin=Pin(lcd_en),
+    d4_pin=Pin(lcd_d4),
+    d5_pin=Pin(lcd_d5),
+    d6_pin=Pin(lcd_d6),
+    d7_pin=Pin(lcd_d7),
+    num_lines=2,
+    num_columns=16
+)
+
+# IR - infravermelho
+
+def receber_sinal(cmd, addr, ctrl):
+    print("botao IR:", cmd)
+
+ir_receiver = NEC_8(ir, receber_sinal)
